c8a4e50edd2fbf69ad62549c1e8120c2ae4b2c30
Walter Soares Costa Neto <contato.walter@proton.me>
2025-08-19 10:25:51 -0300

    feat: create list to save tax payers

--- atividade_02 - 2025-08-19/Main.java
+++ atividade_02 - 2025-08-19/Main.java
@@ -1,5 +1,7 @@
 // Data de entrega: 19/08/2025
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main
@@ -7,6 +9,8 @@ public class Main
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+
+        List<Pessoa> pessoas = new ArrayList<>();
 
         int numTaxPayers;
         char indOrComp; // Individual or Company
@@ -35,13 +39,28 @@ public class Main
             {
                 System.out.print("Health Expenditures: ");
                 healthExpenditures = sc.nextDouble();
+                pessoas.add(new PessoaFisica(name, annualIncome, healthExpenditures));
             }
             else
             {
                 System.out.print("Number of employees: ");
                 numEmployees = sc.nextInt();
+                pessoas.add(new PessoaJuridica(name, annualIncome, numEmployees));
             }
         }
+
+        System.out.println();
+
+        System.out.println("TAXES PAID:");
+        double total = 0;
+        for(Pessoa p : pessoas)
+        {
+            System.out.println(p.getName() + ": $" + String.format("%.2f", p.calculateTax()));
+            total += p.calculateTax();
+        }
+
+        System.out.println();
+        System.out.println("TOTAL TAXES: $" + String.format("%.2f", total));
 
         sc.close();
     }
