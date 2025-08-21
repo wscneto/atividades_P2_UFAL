e9a7eb7e59e010a57d1f1c468fd61ec51bd459e8
Walter Soares Costa Neto <contato.walter@proton.me>
2025-08-20 21:20:27 -0300

    feat: implement first problem from atividade_03

--- atividade_03 - 2025-08-21/problema_1/Main.java
+++ atividade_03 - 2025-08-21/problema_1/Main.java
@@ -2,18 +2,49 @@
 
 import java.util.Scanner;
 import java.time.LocalDate;
-import java.time.DateTimeFormatter;
+import java.time.format.DateTimeFormatter;
 
 public class Main
 {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         int roomNumber;
+        LocalDate checkin;
+        LocalDate checkout;
 
         System.out.print("Room number: ");
         roomNumber = sc.nextInt();
+
+        System.out.print("Check-in date (dd/MM/yyyy): ");
+        checkin = LocalDate.parse(sc.next(), fmt);
+
+        System.out.print("Check-out date (dd/MM/yyyy): ");
+        checkout = LocalDate.parse(sc.next(), fmt);
+
+        try
+        {
+            Reservation res = new Reservation(roomNumber, checkin, checkout);
+            System.out.println(res);
+
+            System.out.println();
+            System.out.println("Enter data to update the reservation:");
+
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            checkin = LocalDate.parse(sc.next(), fmt);
+
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            checkout = LocalDate.parse(sc.next(), fmt);
+        
+            res.updateDates(checkin, checkout);
+            System.out.println(res);
+        }
+        catch(IllegalArgumentException e)
+        {
+            System.out.println(e.getMessage());
+        }
 
         sc.close();
     }
--- atividade_03 - 2025-08-21/problema_1/Reservation.java
+++ atividade_03 - 2025-08-21/problema_1/Reservation.java
@@ -1,21 +1,51 @@
 // Data de entrega: 21/08/2025
 
 import java.time.LocalDate;
-import java.time.DateTimeFormatter;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Reservation
 {
     int roomNumber;
     LocalDate checkin;
     LocalDate checkout;
+    private DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public Reservation(int roomNumber, LocalDate checkin, LocalDate checkout)
     {
+        if(checkin.isAfter(checkout))
+        {
+            throw new IllegalArgumentException("Error in reservation: Check-out date must be after check-in date");
+        }
+
         this.roomNumber = roomNumber;
         this.checkin = checkin;
         this.checkout = checkout;
     }
 
-    public int duration();
-    public void updateDates(LocalDate checkin, LocalDate checkout);
+    public long duration() // ChronoUnit returns long...
+    {
+        return ChronoUnit.DAYS.between(checkin, checkout);
+    }
+
+    public void updateDates(LocalDate checkin, LocalDate checkout)
+    {
+        if(checkin.isBefore(LocalDate.now()) || checkout.isBefore(LocalDate.now()))
+        {
+            throw new IllegalArgumentException("Error in reservation: Reservation dates for update must be future dates");
+        }
+        else if(checkin.isAfter(checkout))
+        {
+            throw new IllegalArgumentException("Error in reservation: Check-out date must be after check-in date");
+        }
+
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    @override
+    public String toString()
+    {
+        return "Reservation: Room " + this.roomNumber + ", check-in: " + checkin.format(fmt) + ", checkout: " + checkout.format(fmt) + ", " + duration() + " nights";
+    }
 }
